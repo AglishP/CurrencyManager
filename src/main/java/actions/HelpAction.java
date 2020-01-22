601bd73fa11e107fc9768b56a91bcdab2f6b7be3
pavelaglish <Aa82300079>
2020-01-22 23:54:55 +0300

    add command description to help

--- src/main/java/actions/HelpAction.java
+++ src/main/java/actions/HelpAction.java
@@ -8,16 +8,20 @@ public class HelpAction extends AbstractAction {
         StringBuilder sb = new StringBuilder();
         sb.append("***************************\n")
         .append("* add - Add new transaction step by step over terminal question *\n")
-                .append("* a - Add new transaction by printing all arguments in one line *\n")
+                .append("* a - Command for Add new transaction by printing all arguments in one line *\n")
                 .append("* in predefine positions: *\n")
-                .append("* - type from or to*\n")
-                .append("* - CurrencyType currency from *\n")
-                .append("* - double amount from *\n")
-                .append("* - CurrencyType currency to*\n")
-                .append("* - boolean use rate or amount to *\n")
-                .append("* - double value of rate or amount *\n")
-                .append("* - WalletType type: cash or debit*\n")
-                .append("*example: a from rub 70000 eur yes 70 debit");
+                .append("*   - type from or to*\n")
+                .append("*   - CurrencyType currency from *\n")
+                .append("*   - double amount from *\n")
+                .append("*   - CurrencyType currency to*\n")
+                .append("*   - boolean use rate or amount to *\n")
+                .append("*   - double value of rate or amount *\n")
+                .append("*   - WalletType type: cash or debit*\n")
+                .append("*   example: a from rub 70000 eur yes 70 debit")
+                .append("___________________________________________")
+                .append("* minus - Command for remove transaction pypeline. Just minus")
+                .append("* view - Command for view all transactions")
+                .append("* summary - Command for smart report");
         Output.showMessage(sb.toString());
     }
 }
